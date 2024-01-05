9ae7dfce3ce686b463107116862af29d26e251b1
Koushik Roy <92144539+koushik173@users.noreply.github.com>
2024-01-05 18:53:08 +0600

    data_Ingestion_added

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from chestCancerPrediction import logger
+# logger.info("welcome to mlflow..")
 
-logger.info("welcome to mlflow..")
+from chestCancerPrediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
